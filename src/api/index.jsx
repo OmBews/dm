c802adfb97b6e6d831868c34564cf555511d565c
fatihjr9 <malikfatihibrahim@gmail.com>
2023-11-13 21:41:39 +0700

    first commit

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,11 +1,11 @@
-import axios from "axios";
+// import axios from "axios";
 
-const Api = axios.create({
-    baseURL: 'https://vip-reseller.co.id/api',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    }
-})
+// const Api = axios.create({
+//     baseURL: 'https://vip-reseller.co.id/api',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json',
+//     }
+// })
 
-export default Api
+// export default Api
